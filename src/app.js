a24d62fd05ec591cd8c6d474ed5e6699b28678dd
Elias Hansen <pe_lias@msn.com>
2016-03-19 15:30:58 +0100

    added firebase cdn and removed from node modules

--- src/app.js
+++ src/app.js
@@ -1,3 +1,24 @@
-/**
- * Created by Eli on 2016-03-19.
- */
+var app = angular.module('builderApp', ['ui.router', 'firebase']);
+
+
+// factory that generates the $firebaseAuth instance
+app.factory("FireAuth", ["$firebaseAuth", function($firebaseAuth) {
+    var ref = new Firebase("https://flower.firebaseio.com/");
+    return $firebaseAuth(ref);
+}]);
+
+// factory that generates the $firebase reference
+app.factory("FireRef", ["$firebase", function($firebase) {
+    var ref = new Firebase("https://flower.firebaseio.com/");
+    return ref;
+}]);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('main', {
+            url: '/main',
+            templateUrl: 'partials/main/main.html'
+        });
+
+    $urlRouterProvider.otherwise('/main/search');
+});
