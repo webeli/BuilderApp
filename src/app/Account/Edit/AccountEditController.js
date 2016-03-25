bf57b5d49715bf0a769a65967c7a54fad9b5dde4
Elias Hansen <pe_lias@msn.com>
2016-03-25 13:48:01 +0100

    accordion changes

--- src/app/Account/Edit/AccountEditController.js
+++ src/app/Account/Edit/AccountEditController.js
@@ -1,4 +1,4 @@
-app.controller('AccountEditController', function($scope, FireRef, $stateParams, $firebaseArray, $uibModal) {
+app.controller('AccountEditController', function($scope, FireRef, $stateParams, $firebaseArray, $uibModal, $timeout) {
     console.log("AccountEditController");
 
     var projectKey = $stateParams.projectKey;
@@ -6,18 +6,22 @@ app.controller('AccountEditController', function($scope, FireRef, $stateParams, 
     // Base ref for project
     var FireProjectRef = FireRef.child(projectKey);
 
-    // Push
+    // Firebase references
+    // ex: var valueRef = new Firebase("https://100meter.firebaseio.com/").child("value");
     var projectCategoryRef = FireProjectRef.child("categories");
     var projectCategoryItemsRef = FireProjectRef.child("categoryItems");
     var projectItemOptionsRef = FireProjectRef.child("itemOptions");
 
-    // Get
+    // Get as array
     var projectCategoryArray = $firebaseArray(FireProjectRef.child("categories"));
     var projectCategoryItemsArray = $firebaseArray(FireProjectRef.child("categoryItems"));
     var projectItemOptionsArray = $firebaseArray(FireProjectRef.child("itemOptions"));
 
-    console.log("#1 ", $firebaseArray);
+    // Starting $scope variables
+    $scope.oneAtATime = true;
+    $scope.categories = projectCategoryArray;
 
+    // Add category
     $scope.addCategory = function () {
         var data = prompt("Ange något");
         projectCategoryRef.push({
@@ -25,41 +29,44 @@ app.controller('AccountEditController', function($scope, FireRef, $stateParams, 
         });
     };
 
+    // Add category item
     $scope.addItem = function (id) {
         var data = prompt("Ange något");
+        var categoryItemsRef = projectCategoryItemsRef.push();
+        var categoryItemsKey = categoryItemsRef.key();
 
-        var newMessageRef = projectCategoryItemsRef.push();
-        var key = newMessageRef.key();
+        // Add reference key to category and data to categoryItems node
+        categoryItemsRef.set({title: data});
+        projectCategoryRef.child(id).child("refs").child(categoryItemsKey).set(true);
 
-        console.log(key);
+    };
 
-        newMessageRef.set({
-            title: data
+    // Get category items
+    $scope.getItems = function(key) {
+        // Store data as object and use in scope
+        $scope.categoryItems = {};
+
+        // Get all category item keys
+        var categoryKeyRefs = FireProjectRef.child("categories").child(key).child("refs");
+
+        // Iterate through all keys from "categoryKeyRefs" and get data from "projectCategoryItemsRef"
+        categoryKeyRefs.on('child_added', function(snapshot) {
+            var itemKey = snapshot.key();
+            projectCategoryItemsRef.child(itemKey).on('value', function(snapshot) {
+                $timeout(function() {
+                    if( snapshot.val() === null ) {
+                        delete $scope.categoryItems[itemKey];
+                    }
+                    else {
+                        $scope.categoryItems[itemKey] = snapshot.val();
+                    }
+                });
+            });
         });
-
-        projectCategoryRef.child(id).child("refs").child(key).set(true);
-
     };
 
-    $scope.getItems = function(id) {
-
-
-        var refArray = $firebaseArray(FireProjectRef.child("categories").child(id).child("refs"));
-
-        console.log(refArray);
-
-        $scope.categoryItems = refArray;
+    $scope.enterCategoryItem = function() {
+        console.log("Enter");
     };
-
-    /*$scope.getItem = function (id) {
-        var result = $firebaseArray(FireRef.child(projectKey).child("categories").child(id).child("items"));
-        $scope.categoryItems = result;
-
-    };*/
-
-    $scope.oneAtATime = true;
-
-    $scope.categories = projectCategoryArray;
-
 
 });
