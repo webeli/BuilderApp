cda5fc7dde77c6357aefcb0e79b18ae65ab73ead
salmin89 <salmin89@hotmail.com>
2016-03-26 03:16:58 +0100

    Added a project view

--- src/app/Project/ProjectController.js
+++ src/app/Project/ProjectController.js
@@ -1,9 +1,13 @@
-app.controller('ProjectController', function($scope, FireRef, $stateParams, $state, $firebaseObject) {
+app.controller('ProjectController', function($scope, FireRef, $stateParams, $state, $firebaseArray, $timeout) {
     console.log("ProjectController");
 
-    var projectRef = FireRef.child($stateParams.projectKey);
+    // KEY
+    var projectKey = $stateParams.projectKey;
+    // Base ref for project
+    var FireProjectRef = FireRef.child(projectKey);
 
-    projectRef.once("value", function(snapshot) {
+
+    FireProjectRef.once("value", function(snapshot) {
         var projectKey = snapshot.exists();
         if (projectKey === false) {
             $state.go("landing");
@@ -13,8 +17,88 @@ app.controller('ProjectController', function($scope, FireRef, $stateParams, $sta
         }
     });
 
-    var values = $firebaseObject(FireRef.child($stateParams.projectKey).child("categories"));
-    console.log(values);
-    $scope.values = values;
 
+
+
+    $scope.completeArray = {Categories: []};
+
+
+    FireProjectRef.child("categories").once("value", function(snapshot) {
+        // The callback function will get called twice, once for "fred" and once for "barney"
+
+        snapshot.forEach(function(childSnapshot) {
+
+            // key will be "fred" the first time and "barney" the second time
+            var key = childSnapshot.key();
+            // childData will be the actual contents of the child
+            var childData = childSnapshot.val();
+
+
+            var tempArray = [];
+
+            FireProjectRef.child("categories").child(key).child("refs").once("value", function(snapshot) {
+
+                snapshot.forEach(function(childSnapshot) {
+                    var key = childSnapshot.key();
+
+                    var test = FireRef.child($stateParams.projectKey).child("categoryItems").child(key);
+
+                    test.once("value", function(snap) {
+                        tempArray.push(snap.val());
+                    });
+
+
+                    //var key = snapshot.val();
+                    //console.log(key);
+
+                });
+            });
+
+            $scope.completeArray.Categories.push({Category: childData, Items: tempArray});
+
+
+        });
+    });
+
+    // Get the options for an item
+    $scope.getOptions = function(key, categoryName, itemName) {
+        // Store data as object and use in scope
+        $scope.imgCategory = categoryName;
+        $scope.imgItem = itemName;
+        $scope.itemOptions = {};
+
+        // Get all category item keys
+        var categoryItemKeyRefs = FireProjectRef.child("categoryItems").child(key).child("refs");
+
+        // Iterate through all keys from "categoryKeyRefs" and get data from "projectCategoryItemsRef"
+        categoryItemKeyRefs.on('child_added', function(snapshot) {
+            var itemKey = snapshot.key();
+            FireProjectRef.child("itemOptions").child(itemKey).on('value', function(snapshot) {
+                $timeout(function() {
+                    if( snapshot.val() === null ) {
+                        delete $scope.itemOptions[itemKey];
+                    }
+                    else {
+                        $scope.itemOptions[itemKey] = snapshot.val();
+                    }
+                });
+            });
+        });
+    };
+});
+
+//Show only active ones
+app.filter('onlyActive', function () {
+    return function (items) {
+        var result = [];
+        angular.forEach(items, function (value, key) {
+
+            if (value.active === true) {
+                result.push(value);
+            }
+
+        });
+        return result;
+
+    }
 });
