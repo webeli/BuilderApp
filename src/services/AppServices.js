1d7c6b2e57a0b659d634ba883ef01cfb071978c9
salmin89 <salmin89@hotmail.com>
2016-04-14 00:00:32 +0200

    Added app.factory for formatPrice

--- src/services/AppServices.js
+++ src/services/AppServices.js
@@ -3,6 +3,14 @@ app.factory("GetChildElementsByKey", ["$timeout", function($timeout) {
     return ref;
 }]);
 
+app.factory('formatPrice', function() {
+    return {
+        foo: function() {
+            alert("I'm foo!");
+        }
+    };
+});
+
 // READ UP ON
 //http://stackoverflow.com/questions/21776237/angularjs-pass-variables-in-service
 //https://www.firebase.com/docs/web/libraries/angular/guide/extending-services.html
