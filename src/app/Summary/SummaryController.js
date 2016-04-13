c3fc2d34779b9a5c80a350174baf7875d1654276
Elias Hansen <pe_lias@msn.com>
2016-04-13 23:00:58 +0200

    added pdfjs to npm, implemented downloadPDF example to project summary

--- src/app/Summary/SummaryController.js
+++ src/app/Summary/SummaryController.js
@@ -13,4 +13,30 @@ app.controller('SummaryController', function($scope, $state, $stateParams, FireR
         }
     }
 
+    $scope.downloadPDF = function() {
+        if ($scope.cart.length === 0) {
+            return;
+        }
+
+        // Generate new jsPDF document
+        var doc = new jsPDF();
+
+        // Title
+        doc.setFontSize(20);
+        doc.setFontType("bold");
+        doc.text(20, 20, 'Sammanfattning');
+
+        // Generating all elements
+        doc.setFontSize(14);
+        doc.setFontType("normal");
+
+        // I know, a pretty uggly for loop
+        for (i = 3; i < $scope.cart.length+3; i++) {
+            doc.text(20, 10*i, $scope.cart[i-3].categoryTitle + ' - '+ $scope.cart[i-3].title + ' Pris: ' + $scope.cart[i-3].price + ':-');
+        }
+
+        // Saving pdf
+        doc.save('Sammanfattning.pdf');
+    };
+
 });
