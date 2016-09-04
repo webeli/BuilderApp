aa19f9308661379c94cad7856dc8deedd0226f13
Elias Hansen <pe_lias@msn.com>
2016-09-04 13:55:34 +0200

    updated to firebase 3.3.0

--- src/services/FirebaseService.js
+++ src/services/FirebaseService.js
@@ -1,14 +1,8 @@
 module.exports = function(app) {
-    // factory that generates the $firebaseAuth instance
-    app.factory('FireAuth', ['$firebaseAuth', function($firebaseAuth) {
-        var ref = new Firebase("https://100meter.firebaseio.com/");
-        return $firebaseAuth(ref);
-    }]);
 
     // factory that generates the $firebase reference
     app.factory('FireRef', ['$firebase', function($firebase) {
-        var ref = new Firebase("https://100meter.firebaseio.com/");
-        return ref;
+        return firebase.database().ref();
     }]);
 
 }
